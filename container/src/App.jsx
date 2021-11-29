aede960c480a6adc0e525a865fd8cb6b432ea799
alexgul1 <alexgulitskyy3@gmail.com>
2021-11-29 18:45:29 +0200

    init project

---
+++ container/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+
+const App = () => (
+    <>
+        <h1>Hi there!</h1>
+        <MarketingApp />
+    </>)
+
+export default App;
